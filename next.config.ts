eb056e649c86751220a7ce209ff0033c45138db9
Ravesss-dev04 <animeteevi@gmail.com>
2025-10-24 16:34:59 +0800

    bbbsitesv-22223223

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+   images: {
+    domains: ['raw.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '**',
+      },
+    ],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
