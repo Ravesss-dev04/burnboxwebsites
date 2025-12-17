fa27f4bba2bdafc8885cf5c483dc95bbda54300c
Ravesss-dev04 <animeteevi@gmail.com>
2025-12-17 11:43:53 +0800

    2026wewe

--- app/components/StoreLocatorPage.tsx
+++ app/components/StoreLocatorPage.tsx
@@ -1,21 +1,26 @@
 import React, { useState } from 'react'
 import StoreDetail from './StoreDetail';
 import StoreLocation from './StoreLocation';
+import { Branch, branches } from '@/data/branches';
 
 const StoreLocatorPage = () => {
     const [target, setTarget] = useState<[number, number] | null>(null);
+    // Default to the first branch for now since the UI only shows one detail view
+    const currentBranch = branches[0];
 
     const handleDirectionClick = () => {
-        setTarget([14.428425252312016, 120.98849405250161]);
+        setTarget([currentBranch.coordinates.lat, currentBranch.coordinates.lng]);
     };
 
     return (
         <div className='flex flex-col md:flex-row h-full w-full'>
             <div className='w-full md:w-1/3 p-4 bg-black'>
-                <StoreDetail onDirectionClick={handleDirectionClick}/>
+                <StoreDetail branch={currentBranch} onDirectionClick={handleDirectionClick}/>
             </div>
             <div className='w-full md:w-2/3 h-[500px]'>
-                <StoreLocation target={target || undefined} />
+                <StoreLocation target={target || undefined} branches={[]} onSelectBranch={function (branch: Branch): void {
+                    throw new Error('Function not implemented.');
+                } } />
             </div>
         </div>
     )
